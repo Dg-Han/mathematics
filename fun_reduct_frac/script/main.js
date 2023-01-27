de6509229bd8a209897b40914e8b8820ff690f9a
Dg_Han <101265358+Dg-Han@users.noreply.github.com>
2023-01-27 23:37:19 +0800

    Update files

--- fun_reduct_frac/script/main.js
+++ fun_reduct_frac/script/main.js
@@ -1,6 +1,7 @@
 /* this js file relys on jquery*/
 
-var fun_frac_data, fun_frac_keys, data_length
+var fun_frac_data, fun_frac_keys, data_length, i;
+var detail_hint = ["Show details", "What?!", "Really?", "Is it true...?"]
 
 $.ajax({
     type:"GET",
@@ -32,12 +33,29 @@ function get_reduct_result(raw, res){
     return str
 }
 
-function get_fun_frac(){
-    let i = Math.floor(Math.random() * data_length);
-    var key_str = fun_frac_keys[i]
+function random_choice(l){
+    let j = Math.floor(Math.random() * detail_hint.length);
+    return l[j]
+}
 
-    var fun_frac_raw_str = key_str.split(",");
+function get_fun_frac(){
+    $("#detail_pic").hide()
+    i = Math.floor(Math.random() * data_length);
+    var key_str = fun_frac_keys[i];                     // 原分数
+
+    var fun_frac_raw_str = key_str.split(",");          // 错约结果
     var fun_frac_str = [get_reduct_result(fun_frac_raw_str[0], fun_frac_data[key_str][0].toString()), get_reduct_result(fun_frac_raw_str[1], fun_frac_data[key_str][1].toString())];
 
-    $("#math_pic").html(`<img src=\"https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_str[0]}}{${fun_frac_str[1]}}\=\\dfrac{${fun_frac_data[key_str][0]}}{${fun_frac_data[key_str][1]}}\">`);
+    $("#math_pic").html(`<img src=\"https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=\\dfrac{${fun_frac_str[0]}}{${fun_frac_str[1]}}\=\\dfrac{${fun_frac_data[key_str][0]}}{${fun_frac_data[key_str][1]}}\" alt="获取中，请稍后...">`);
+    $("#detail_hint").html(`${random_choice(detail_hint)}`)
+    $("#detail_hint").show()
+}
+
+function show_details(){
+    var key_str = fun_frac_keys[i];
+    var fun_frac_raw_str = key_str.split(",");
+    var common_factor = Math.round(Number(fun_frac_raw_str[0]) / Number(fun_frac_data[key_str][0])).toString();
+    $("#detail_pic").html(`<img src=\"https://latex.codecogs.com/svg.image?\=\\dfrac{${fun_frac_data[key_str][0]}\\times${common_factor}}{${fun_frac_data[key_str][1]}\\times${common_factor}}\">`)
+    $("#detail_hint").hide()
+    $("#detail_pic").show()
 }
