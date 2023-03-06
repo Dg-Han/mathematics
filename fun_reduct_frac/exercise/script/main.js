9a67a38bfe47b3dbca5c83df609c9183239d0c1c
Dg_Han <101265358+Dg-Han@users.noreply.github.com>
2023-03-06 13:02:54 +0800

    Update files via upload

--- fun_reduct_frac/exercise/script/main.js
+++ fun_reduct_frac/exercise/script/main.js
@@ -12,14 +12,50 @@ $.ajax({
         data_length = fun_frac_keys.length;
         init();
     },
+    error:function(){
+        alert("缓存数据加载失败，请确认网络连接状态后刷新重试")
+    }
 });
 
+function get_latex_pic_html(target_jquery, pic_url, success_func){
+    $.ajax({
+        type:"GET",
+        url: pic_url,
+        dataType:"html",
+        success: function(data){
+            $(`${target_jquery}`).html(data);
+            statu = true;
+            success_func();
+        },
+    })
+}
+
 function init(){
+    $(".frac_input").hide();
+    $("#btn").hide();
+    $("#frac").html("题目加载中，请稍后...");
+
     i = Math.floor(Math.random() * data_length);
     var key_str = fun_frac_keys[i];                     // 原分数
     var fun_frac_raw_str = key_str.split(",");
 
-    $("#frac").html(`<img src=\"https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=" alt="获取中，请稍后...">`)
+    get_latex_pic_html("#frac", `https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=`, 
+                       function(){
+        $(".frac_input").show();
+        $("#btn").show();
+    });
+    /*
+    $.ajax({
+        type:"GET",
+        url:`https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=`,
+        dataType:"html",
+        success: function(data){
+            $("#frac").html(data);
+            $(".frac_input").show();
+            $("#btn").show();
+        }
+    });
+    */
 }
 
 function get_reduct_result(raw, res){
@@ -42,6 +78,7 @@ function get_reduct_result(raw, res){
 }
 
 function check(){
+    $("#react").html("获取解析中...")
     var num = $("#num").val();
     var denom = $("#denom").val();
     var key_str = fun_frac_keys[i];                     // 原分数
@@ -52,12 +89,13 @@ function check(){
         alert("Correct!")
         var fun_frac_str = [get_reduct_result(fun_frac_raw_str[0], fun_frac_data[key_str][0].toString()), get_reduct_result(fun_frac_raw_str[1], fun_frac_data[key_str][1].toString())];
 
-        $("#react").html(`<img src=\"https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=\\dfrac{${fun_frac_str[0]}}{${fun_frac_str[1]}}\=\\dfrac{${fun_frac_data[key_str][0]}}{${fun_frac_data[key_str][1]}}\" alt="获取中，请稍后...">`);
+        get_latex_pic_html("#react", `https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=\\dfrac{${fun_frac_str[0]}}{${fun_frac_str[1]}}\=\\dfrac{${fun_frac_data[key_str][0]}}{${fun_frac_data[key_str][1]}}`, function(){return});
     }
     else{
         alert("Wrong!")
-        var common_factor = Math.round(Number(fun_frac_raw_str[0]) / Number(fun_frac_data[key_str][0])).toString()
-        $("#react").html(`<img src=\"https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=\\dfrac{${fun_frac_data[key_str][0]}\\times${common_factor}}{${fun_frac_data[key_str][1]}\\times${common_factor}}\=\\dfrac{${fun_frac_data[key_str][0]}}{${fun_frac_data[key_str][1]}}\" alt="获取中，请稍后...">`);
+        var common_factor = Math.round(Number(fun_frac_raw_str[0]) / Number(fun_frac_data[key_str][0])).toString();
+
+        get_latex_pic_html("#react", `https://latex.codecogs.com/svg.image?\\dfrac{${fun_frac_raw_str[0]}}{${fun_frac_raw_str[1]}}\=\\dfrac{${fun_frac_data[key_str][0]}\\times${common_factor}}{${fun_frac_data[key_str][1]}\\times${common_factor}}\=\\dfrac{${fun_frac_data[key_str][0]}}{${fun_frac_data[key_str][1]}}`, function(){return});
     }
 }
 
@@ -70,6 +108,10 @@ function handling(){
         $("#react").html("");
     }
     else if ($("#btn").html() == "check"){
+        if ($("#frac").html() == "题目加载中，请稍后..."){
+            alert("题目尚未加载完成，请稍后再试");
+            return
+        };
         check();
         $("#btn").html("下一题");
     }
